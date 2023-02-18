132a45fd1baf7e01d6bc8ac3d8a4e72b888cb295
Azeem0300033 <muhammadazeem0300033@gmail.com>
2023-02-18 15:14:32 +0500

    mail fixes

---
+++ send-mail.php
@@ -0,0 +1,44 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
+require 'vendor/phpmailer/phpmailer/src/Exception.php';
+require 'vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require 'vendor/phpmailer/phpmailer/src/SMTP.php';
+
+$mail = new PHPMailer(true);
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['sub'];
+$phone = $_POST['phone'];
+$msg = $_POST['msg'];
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 1;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'bixcash.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'welcome@bixcash.com';                     //SMTP username
+    $mail->Password   = 'bixcash_email';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;                                    //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('welcome@bixcash.com');
+    $mail->addAddress('dev.muhammadazeem@gmail.com', 'Azeem');
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = $subject;
+    $mail->Body    = 'Massgae : ' . $msg ;
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    header('Location: index.html');
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
